f2c0b08f9c21280894de5a013506d79779179089
Sudhakar Kuppusamy <sudhakar@linux.ibm.com>
2025-07-05 08:29:41 +0530

    powerpc_ieee1275: Read the db and dbx secure boot variables
    Enhancing the infrastructure to enable the Platform Keystore (PKS) feature,
    which provides access to the SB_VERSION, db, and dbx secure boot variables
    from PKS.
    
    If secure boot is enabled with dynamic key management mode, it will read
    secure boot variables such as db and dbx from PKS and extract
    EFI Signature List (ESL) from it. The ESLs would be saved in the
    Platform Keystore buffer, and the appendedsig module would read it later
    to extract the certificate's details from ESL.
    
    In the following scenarios, static key management mode will be activated:
     1. When Secure Boot is enabled with static key management mode
     2. When SB_VERSION is unavailable but Secure Boot is enabled
     3. When PKS support is unavailable but Secure Boot is enabled
    
    Note:-
    
     SB_VERSION: Key Management Mode
     1 - Enable dynamic key management mode. Read the db and dbx variables from PKS,
         and use them for signature verification.
     0 - Enable static key management mode. Read keys from the GRUB ELF Note and
         use it for signature verification.
    
    Signed-off-by: Sudhakar Kuppusamy <sudhakar@linux.ibm.com>

--- include/grub/powerpc/ieee1275/ieee1275.h
+++ include/grub/powerpc/ieee1275/ieee1275.h
@@ -28,4 +28,24 @@ typedef grub_uint32_t grub_ieee1275_cell_t;
 #define PRIxGRUB_IEEE1275_CELL_T	PRIxGRUB_UINT32_T
 #define PRIuGRUB_IEEE1275_CELL_T	PRIuGRUB_UINT32_T
 
+#ifdef __powerpc__
+
+extern grub_int32_t
+grub_ieee1275_test (const grub_uint8_t *name, grub_ieee1275_cell_t *missing);
+
+extern grub_int32_t
+grub_ieee1275_pks_max_object_size (grub_ieee1275_cell_t *result);
+
+extern grub_int32_t
+grub_ieee1275_pks_read_object (const grub_uint8_t consumer, const grub_uint8_t *label,
+                               const grub_size_t label_len, const grub_size_t buffer_len,
+                               grub_uint8_t *buffer, grub_uint32_t *data_len,
+                               grub_uint32_t *policies);
+
+extern grub_int32_t
+grub_ieee1275_pks_read_sbvar (const grub_uint8_t sbvar_flags, const grub_uint8_t sbvar_type,
+                              const grub_uint32_t buffer_len, grub_uint8_t *buffer,
+                              grub_size_t *data_len);
+#endif
+
 #endif /* ! GRUB_IEEE1275_MACHINE_HEADER */
