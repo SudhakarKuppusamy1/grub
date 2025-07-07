9a2c434ac7f8aeeea98c4435c4270fa7bf6ff959
Sudhakar Kuppusamy <sudhakar@linux.ibm.com>
2025-07-07 20:47:33 +0530

    powerpc_ieee1275: Introduce use_static_keys flag
    Introduce the use_static_keys flag to indicate that static keys are to be used
    rather than keys from the PKS storage's db variable. This flag is set when
    Secure Boot is enabled with PKS but the db variable is not present in the PKS storage.
    The appendedsig module would use this flag to extract the static keys from
    the GRUB ELF Note and stored in the db list.
    
    Signed-off-by: Sudhakar Kuppusamy <sudhakar@linux.ibm.com>
    Reviewed-by: Stefan Berger <stefanb@linux.ibm.com>
    Reviewed-by: Avnish Chouhan <avnish@linux.ibm.com>
    Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>

--- grub-core/kern/powerpc/ieee1275/platform_keystore.c
+++ grub-core/kern/powerpc/ieee1275/platform_keystore.c
@@ -47,7 +47,8 @@ static grub_size_t pks_max_object_size;
 bool grub_pks_use_keystore = false;
 
 /* Platform Keystore. */
-grub_pks_t grub_pks_keystore = { .db = NULL, .dbx = NULL, .db_entries = 0, .dbx_entries = 0 };
+grub_pks_t grub_pks_keystore = { .db = NULL, .dbx = NULL, .db_entries = 0, .dbx_entries = 0,
+                                 .use_static_keys = false };
 
 /*
  * Import the Globally Unique Identifier (GUID), EFI Signature Database (ESD),
@@ -316,6 +317,15 @@ grub_pks_keystore_init (void)
       grub_memset (&grub_pks_keystore, 0, sizeof (grub_pks_t));
       /* Read db from PKS. */
       rc = read_secure_boot_variables (0, DB, &grub_pks_keystore.db, &grub_pks_keystore.db_entries);
+      if (rc == GRUB_ERR_UNKNOWN_COMMAND)
+        {
+          rc = GRUB_ERR_NONE;
+          /*
+           * The db variable won't be available by default in PKS.
+           * So, it will use the static key as a default key from the GRUB ELF Note.
+           */
+          grub_pks_keystore.use_static_keys = true;
+        }
       if (rc == GRUB_ERR_NONE)
         {
           /* Read dbx from PKS. */
