fb4ee9b2e41835f4567706cbdfdc63b3072eab3e
Sudhakar Kuppusamy <sudhakar@linux.ibm.com>
2026-07-22 16:49:23 +0530

    appendedsig/x509: Refactor certificate handling and remove redundant internal structures
    Refactor the appendedsig subsystem to transition from the legacy internal
    'struct x509_certificate' definition to the unified 'grub_x509_cert_t'
    model. This centralizes X.509 definitions and removes duplicate types.
    Key changes include moving definitions to x509.h, replacing MPIs with
    raw SPKI (subject public key info), updating parsing/release functions to use
    'grub_x509_spec' handlers, and prepares the codebase for cleaner post-quantum
    cryptographic extensions.
    
    Key adjustments:
    - Move architecture-independent X.509 definitions from appendedsig.h to x509.h.
    - Replace internal multiprecision integers (gcry_mpi_t) with subject public key info
      (cert->spki.pk) to clean up raw ASN.1 structure representation.
    - Swap standard 'grub_x509_cert_parse' invocations for explicit '_der' variants
      (x509_cert_parse_der) to firmly distinct binary parsing logic.
    - Standardise on 'cert->spki.pk_len' tracking instead of relying on modulus_size calculations.
    
    Signed-off-by: Sudhakar Kuppusamy <sudhakar@linux.ibm.com>

---
+++ grub-core/commands/appendedsig/x509.h
@@ -0,0 +1,103 @@
+/*
+ *  GRUB  --  GRand Unified Bootloader
+ *  Copyright (C) 2020, 2022 Free Software Foundation, Inc.
+ *  Copyright (C) 2020, 2022, 2025 IBM Corporation
+ *
+ *  GRUB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GRUB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef X509_H
+#define X509_H
+
+#include <grub/crypto.h>
+#include <libtasn1.h>
+
+/* Certificate fingerprint. */
+#define GRUB_MAX_FINGERPRINT     3
+#define GRUB_FINGERPRINT_SHA256  0
+#define GRUB_FINGERPRINT_SHA384  1
+#define GRUB_FINGERPRINT_SHA512  2
+
+/* Max size of hash data. */
+#define GRUB_MAX_HASH_LEN        64
+
+/* Public Key Algorithm. */
+struct pk_algo
+{
+  const char *name;
+  const char *aliases;
+  const char *oid;
+  const grub_int32_t oid_len;
+};
+typedef struct pk_algo grub_pkalgo_t;
+
+struct pubkey
+{
+  gcry_mpi_t modulus;
+  gcry_mpi_t exponent;
+};
+typedef struct pubkey grub_pk_t;
+
+/* Subject Public Key Info. */
+struct spk_info
+{
+  grub_pkalgo_t pk_algo;
+  grub_pk_t pk;
+  grub_int32_t pk_len;
+};
+typedef struct spk_info grub_spki_t;
+
+/*
+ * One or more x509 certificates. We do limited parsing:
+ * extracting only the version, serial, issuer, subject, RSA public key
+ * and key size.
+ * Also, hold the sha256, sha384, and sha512 fingerprint of the certificate.
+ */
+struct x509_cert
+{
+  grub_uint8_t version;
+  grub_uint8_t *serial;
+  grub_int32_t serial_len;
+  char *issuer;
+  grub_int32_t issuer_len;
+  char *subject;
+  grub_int32_t subject_len;
+  grub_spki_t spki;
+  grub_uint8_t fingerprint[GRUB_MAX_FINGERPRINT][GRUB_MAX_HASH_LEN];
+  struct x509_cert *next;
+};
+typedef struct x509_cert grub_x509_cert_t;
+
+/* Type for the x509_cert_parse_der function.  */
+typedef grub_err_t (*grub_x509_parse_t) (const void *der_data,
+                                         const grub_int32_t der_data_len,
+                                         grub_x509_cert_t *cert);
+
+/* Type for the x509_cert_release function.  */
+typedef void (*grub_x509_release_t) (grub_x509_cert_t *cert);
+
+/* Type for the x509_cert_free function.  */
+typedef void (*grub_x509_free_t) (grub_x509_cert_t *cert);
+
+typedef struct x509_spec
+{
+  const char *name;
+  grub_x509_parse_t parse_der;
+  grub_x509_release_t release;
+  grub_x509_free_t free;
+} grub_x509_spec_t;
+
+extern grub_x509_spec_t *grub_x509_spec;
+
+#endif /* X509_H */
