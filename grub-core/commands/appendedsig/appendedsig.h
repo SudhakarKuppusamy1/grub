9004faa85c59dcae02b0dbb0770bed6c144f6960
Sudhakar Kuppusamy <sudhakar@linux.ibm.com>
2026-07-23 23:02:49 +0530

    appendedsig/pkcs7: modernize and isolate PKCS#7 parsing types
    Drop the duplicate, legacy 'struct pkcs7_data' block from appendedsig.h
    and move the cryptographic data layouts to a dedicated 'pkcs7.h' header.
    This unifies type definitions using the new 'grub_pkcs7_signed_data_t'
    framework across the core appendedsig verification pipeline, updating
    parsing/release/free functions to use 'grub_pkcs7_spec' handlers, and
    prepares the codebase for cleaner post-quantum cryptographic extensions.
    
    - Introduce pkcs7.h to encapsulate structure definitions
    - Convert 'grub_pkcs7_data_parse' to 'grub_pkcs7_signed_data_parse_der'
    - Use 'grub_mdalgo_t digest_algo' for dynamic hash context handling
    - Map signatures via 'si->signature' instead of the generic 'sig_mpi'
    
    Signed-off-by: Sudhakar Kuppusamy <sudhakar@linux.ibm.com>

--- grub-core/commands/appendedsig/appendedsig.h
+++ grub-core/commands/appendedsig/appendedsig.h
@@ -19,36 +19,3 @@
 
 #include <grub/crypto.h>
 
-/* A PKCS#7 signed data signer info. */
-struct pkcs7_signer
-{
-  const gcry_md_spec_t *hash;
-  gcry_mpi_t sig_mpi;
-};
-typedef struct pkcs7_signer grub_pkcs7_signer_t;
-
-/*
- * A PKCS#7 signed data message. We make no attempt to match intelligently, so
- * we don't save any info about the signer.
- */
-struct pkcs7_data
-{
-  grub_int32_t signer_count;
-  grub_pkcs7_signer_t *signers;
-};
-typedef struct pkcs7_data grub_pkcs7_data_t;
-
-/*
- * Parse a PKCS#7 message, which must be a signed data message. The message must
- * be in 'sigbuf' and of size 'data_size'. The result is placed in 'msg', which
- * must already be allocated.
- */
-extern grub_err_t
-grub_pkcs7_data_parse (const void *sigbuf, grub_size_t data_size, grub_pkcs7_data_t *msg);
-
-/*
- * Release all the storage associated with the PKCS#7 message. If the caller
- * dynamically allocated the message, it must free it.
- */
-extern void
-grub_pkcs7_data_release (grub_pkcs7_data_t *msg);
---
+++ grub-core/commands/appendedsig/pkcs7.h
@@ -0,0 +1,74 @@
+/*
+ *  GRUB  --  GRand Unified Bootloader
+ *  Copyright (C) 2020, 2022 Free Software Foundation, Inc.
+ *  Copyright (C) 2020, 2022, 2025 IBM Corporation
+ *
+ *  GRUB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GRUB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PKCS7_H
+#define PKCS7_H
+
+#include <grub/crypto.h>
+#include "x509.h"
+
+struct md_algo
+{
+  const char *name;
+  const char *oid;
+  const grub_int32_t oid_len;
+  const gcry_md_spec_t *hash;
+};
+typedef struct md_algo grub_mdalgo_t;
+
+/* A PKCS#7 signed data signer info. */
+struct pkcs7_signerInfo
+{
+  grub_mdalgo_t digest_algo;
+  gcry_mpi_t signature;
+  struct pkcs7_signerInfo *next;
+};
+typedef struct pkcs7_signerInfo grub_pkcs7_signer_t;
+
+/* A PKCS#7 signed data. */
+struct pkcs7_signedData
+{
+  grub_int32_t version;
+  grub_mdalgo_t digest_algo;
+  grub_int32_t no_of_signers;
+  grub_pkcs7_signer_t *signers;
+};
+typedef struct pkcs7_signedData grub_pkcs7_signed_data_t;
+
+/* Type for the pkcs7_signed_data_parse_der function.  */
+typedef grub_err_t (*grub_pkcs7_parse_t) (const void *der_data, grub_int32_t der_data_len,
+                                          grub_pkcs7_signed_data_t *pkcs7_signed_data);
+
+/* Type for the pkcs7_signed_data_release function.  */
+typedef void (*grub_pkcs7_release_t) (grub_pkcs7_signed_data_t *pkcs7_signed_data);
+
+/* Type for the pkcs7_signed_data_free function.  */
+typedef void (*grub_pkcs7_free_t) (grub_pkcs7_signed_data_t *pkcs7_signed_data);
+
+typedef struct pkcs7_spec
+{
+  const char *name;
+  grub_pkcs7_parse_t parse_der;
+  grub_pkcs7_release_t release;
+  grub_pkcs7_free_t free;
+} grub_pkcs7_spec_t;
+
+extern grub_pkcs7_spec_t *grub_pkcs7_spec;
+
+#endif /* PKCS7_H */
