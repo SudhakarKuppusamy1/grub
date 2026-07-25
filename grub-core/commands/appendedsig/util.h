ac76f85c884e534922d68be082b6b55e71a387a9
Sudhakar Kuppusamy <sudhakar@linux.ibm.com>
2026-07-25 20:03:21 +0530

    appendedsig: Move print_certificate to x509.c
    Move the `print_certificate` logic out of `appendedsig.c` and into
    `x509.c` as a static function named `x509_print_cert`. Expose this
    functionality across the subsystem by adding a hook to the
    `grub_x509_spec_t` dispatch structure. Additionally, clean up local
    display utilities by replacing the internal `hexdump_colon` helper with
    the shared `grub_util_hexdump_colon` implementation.
    
    Signed-off-by: Sudhakar Kuppusamy <sudhakar@linux.ibm.com>

--- grub-core/commands/appendedsig/util.h
+++ grub-core/commands/appendedsig/util.h
@@ -28,4 +28,23 @@ extern grub_err_t
 grub_util_buffer2hex (const grub_uint8_t *data, const grub_int32_t data_len,
                       grub_uint8_t **hex, grub_int32_t *hex_len);
 
+/*
+ * We cannot use hexdump() to display hash data because it is typically displayed
+ * in hexadecimal format, along with an ASCII representation of the same data.
+ *
+ * Example: sha256 hash data
+ * 00000000  52 b5 90 49 64 de 22 d7  4e 5f 4f b4 1b 51 9c 34  |R..Id.".N_O..Q.4|
+ * 00000010  b1 96 21 7c 91 78 a5 0d  20 8c e9 5c 22 54 53 f7  |..!|.x.. ..\"TS.|
+ *
+ * An appended signature only required to display the hexadecimal of the hash data
+ * by separating each byte with ":". So, we introduced a new method hexdump_colon
+ * to display it.
+ *
+ * Example: Sha256 hash data
+ *  52:b5:90:49:64:de:22:d7:4e:5f:4f:b4:1b:51:9c:34:
+ *  b1:96:21:7c:91:78:a5:0d:20:8c:e9:5c:22:54:53:f7
+ */
+extern void
+grub_util_hexdump_colon (const grub_uint8_t *data, const grub_size_t length);
+
 #endif /* UTIL_H */
--- grub-core/commands/appendedsig/x509.h
+++ grub-core/commands/appendedsig/x509.h
@@ -75,6 +75,9 @@ struct x509_cert
 };
 typedef struct x509_cert grub_x509_cert_t;
 
+/* Type for the x509_print_cert function.  */
+typedef void (*grub_x509_print_t) (const grub_x509_cert_t *crt);
+
 /* Type for the x509_cert_parse_der function.  */
 typedef grub_err_t (*grub_x509_parse_t) (const void *der_data,
                                          const grub_int32_t der_data_len,
@@ -89,6 +92,7 @@ typedef void (*grub_x509_free_t) (grub_x509_cert_t *cert);
 typedef struct x509_spec
 {
   const char *name;
+  grub_x509_print_t print;
   grub_x509_parse_t parse_der;
   grub_x509_release_t release;
   grub_x509_free_t free;
