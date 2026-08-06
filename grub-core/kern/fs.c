SudhakarKuppusamy1/grub

commit ad4bfeec5ca4cc8479e9353c530a7e77f76d26bd

Change fs functions to add fs_ prefix
This avoid conflict with gnulib

Signed-off-by: Vladimir Serbinenko <phcoder@google.com>
Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>

grub-core/kern/fs.c
@@ -64,13 +64,13 @@ grub_fs_probe (grub_device_t device)
 	  if (grub_strcmp (p->name, "btrfs") == 0)
 	    {
 	      char *label = 0;
-	      p->uuid (device, &label);
+	      p->fs_uuid (device, &label);
 	      if (label)
 		grub_free (label);
 	    }
 	  else
 #endif
-	    (p->dir) (device, "/", probe_dummy_iter, NULL);
+	    (p->fs_dir) (device, "/", probe_dummy_iter, NULL);
 	  if (grub_errno == GRUB_ERR_NONE)
 	    return p;
 
@@ -94,7 +94,7 @@ grub_fs_probe (grub_device_t device)
 	    {
 	      p = grub_fs_list;
 
-	      (p->dir) (device, "/", probe_dummy_iter, NULL);
+	      (p->fs_dir) (device, "/", probe_dummy_iter, NULL);
 	      if (grub_errno == GRUB_ERR_NONE)
 		{
 		  count--;
@@ -243,9 +243,9 @@ grub_fs_blocklist_read (grub_file_t file, char *buf, grub_size_t len)
 struct grub_fs grub_fs_blocklist =
   {
     .name = "blocklist",
-    .dir = 0,
-    .open = grub_fs_blocklist_open,
-    .read = grub_fs_blocklist_read,
-    .close = 0,
+    .fs_dir = 0,
+    .fs_open = grub_fs_blocklist_open,
+    .fs_read = grub_fs_blocklist_read,
+    .fs_close = 0,
     .next = 0
   };

grub-core/commands/ls.c
@@ -221,9 +221,9 @@ grub_ls_list_files (char *dirname, int longlist, int all, int human)
       };
 
       if (longlist)
-	(fs->dir) (dev, path, print_files_long, &ctx);
+	(fs->fs_dir) (dev, path, print_files_long, &ctx);
       else
-	(fs->dir) (dev, path, print_files, &ctx);
+	(fs->fs_dir) (dev, path, print_files, &ctx);
 
       if (grub_errno == GRUB_ERR_BAD_FILE_TYPE
 	  && path[grub_strlen (path) - 1] != '/')

grub-core/commands/macbless.c
@@ -159,12 +159,12 @@ grub_mac_bless_file (grub_device_t dev, const char *path_in, int intel)
       *tail = 0;
       ctx.dirname = tail + 1;
 
-      (fs->dir) (dev, *path == 0 ? "/" : path, find_inode, &ctx);
+      (fs->fs_dir) (dev, *path == 0 ? "/" : path, find_inode, &ctx);
     }
   else
     {
       ctx.dirname = path + 1;
-      (fs->dir) (dev, "/", find_inode, &ctx);
+      (fs->fs_dir) (dev, "/", find_inode, &ctx);
     }
   if (!ctx.found)
     {

grub-core/commands/nativedisk.c
@@ -109,7 +109,7 @@ get_uuid (const char *name, char **uuid, int getnative)
       grub_device_close (dev);
       return grub_errno;
     }
-  if (!fs->uuid || fs->uuid (dev, uuid) || !*uuid)
+  if (!fs->fs_uuid || fs->fs_uuid (dev, uuid) || !*uuid)
     {
       grub_device_close (dev);
 

grub-core/commands/pgp.c
@@ -934,7 +934,7 @@ pseudo_read (struct grub_file *file, char *buf, grub_size_t len)
 struct grub_fs pseudo_fs = 
   {
     .name = "pseudo",
-    .read = pseudo_read
+    .fs_read = pseudo_read
   };
 
 struct grub_file_verifier grub_pubkey_verifier =

grub-core/commands/probe.c
@@ -113,10 +113,10 @@ grub_cmd_probe (grub_extcmd_context_t ctxt, int argc, char **args)
   if (state[4].set)
     {
       char *uuid;
-      if (! fs->uuid)
+      if (! fs->fs_uuid)
 	return grub_error (GRUB_ERR_NOT_IMPLEMENTED_YET,
 			   N_("%s does not support UUIDs"), fs->name);
-      err = fs->uuid (dev, &uuid);
+      err = fs->fs_uuid (dev, &uuid);
       if (err)
 	return err;
       if (! uuid)
@@ -134,11 +134,11 @@ grub_cmd_probe (grub_extcmd_context_t ctxt, int argc, char **args)
   if (state[5].set)
     {
       char *label;
-      if (! fs->label)
+      if (! fs->fs_label)
 	return grub_error (GRUB_ERR_NOT_IMPLEMENTED_YET,
 			   N_("filesystem `%s' does not support labels"),
 			   fs->name);
-      err = fs->label (dev, &label);
+      err = fs->fs_label (dev, &label);
       if (err)
 	return err;
       if (! label)

grub-core/commands/search.c
@@ -103,9 +103,9 @@ iterate_device (const char *name, void *data)
 	  fs = grub_fs_probe (dev);
 
 #ifdef DO_SEARCH_FS_UUID
-#define read_fn uuid
+#define read_fn fs_uuid
 #else
-#define read_fn label
+#define read_fn fs_label
 #endif
 
 	  if (fs && fs->read_fn)

grub-core/commands/test.c
@@ -133,15 +133,15 @@ get_fileinfo (char *path, struct test_parse_ctx *ctx)
 
       /* Fetch writing time. */
       ctx->file_info.mtimeset = 0;
-      if (fs->mtime)
+      if (fs->fs_mtime)
 	{
-	  if (! fs->mtime (dev, &ctx->file_info.mtime))
+	  if (! fs->fs_mtime (dev, &ctx->file_info.mtime))
 	    ctx->file_info.mtimeset = 1;
 	  grub_errno = GRUB_ERR_NONE;
 	}
     }
   else
-    (fs->dir) (dev, path, find_file, ctx);
+    (fs->fs_dir) (dev, path, find_file, ctx);
 
   grub_device_close (dev);
   grub_free (path);

commit fc5d56b44425ca0ba59f58a226fe40d083515ef6

kern/fs: Free the block list on file close
The block list filesystem (grub_fs_blocklist) allocates its array of
grub_fs_block structures in grub_fs_blocklist_open() and stores it in
file->data, but registered no fs_close method.  grub_file_close() only
frees file->name and the file structure itself, so the block list array
was leaked on every close of a block-list file -- e.g. "(hd0)0+1", the
classic "chainloader +1", or a loopback over a block list.

Add grub_fs_blocklist_close() to free file->data.  The leak is small per
open but accumulates whenever block-list files are opened repeatedly in a
single GRUB session.

Fixes: ad4bfeec5 ("Change fs functions to add fs_ prefix")

Assisted-by: Claude:Opus-4.8

Signed-off-by: Andrew Hamilton <adhamilt@gmail.com>
Reviewed-by: Leo Sandoval <lsandova@redhat.com>
Reviewed-by: Sudhakar Kuppusamy <sudhakar@linux.ibm.com>
Part-of: <https://gitlab.freedesktop.org/gnu-grub/grub/-/merge_requests/205>

grub-core/kern/fs.c
@@ -256,12 +256,20 @@ grub_fs_blocklist_read (grub_file_t file, char *buf, grub_size_t len)
   return ret;
 }
 
+static grub_err_t
+grub_fs_blocklist_close (grub_file_t file)
+{
+  grub_free (file->data);
+  file->data = 0;
+  return GRUB_ERR_NONE;
+}
+
 struct grub_fs grub_fs_blocklist =
   {
     .name = "blocklist",
     .fs_dir = 0,
     .fs_open = grub_fs_blocklist_open,
     .fs_read = grub_fs_blocklist_read,
-    .fs_close = 0,
+    .fs_close = grub_fs_blocklist_close,
     .next = 0
   };